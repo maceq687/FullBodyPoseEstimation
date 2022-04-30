00c86847eb63ed9965646ef930a7366ab9ba939a
maceq687 <maciek@softman.pl>
2022-04-30 20:21:34 +0200

    Added Support Camera and simple CameraManager script

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class CameraManager : MonoBehaviour
+{
+    public CameraManager CamManager;
+    public Camera mainCamera;
+    private Camera supportCamera;
+    private bool vrRunning;
+    
+    void Start()
+    {
+        supportCamera = CamManager.GetComponent<Camera>();
+        vrRunning = isVrRunning();
+        if (vrRunning)
+            {
+                supportCamera.enabled = false;
+                mainCamera.enabled = true;
+            }
+            else
+            {
+                supportCamera.enabled = true;
+                mainCamera.enabled = false;
+            }
+    }
+
+    private static bool isVrRunning()
+    {
+        var xrDisplaySubsystems = new List<XRDisplaySubsystem>();
+        SubsystemManager.GetInstances<XRDisplaySubsystem>(xrDisplaySubsystems);
+        foreach (var xrDisplay in xrDisplaySubsystems)
+        {
+            if (xrDisplay.running)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+}
