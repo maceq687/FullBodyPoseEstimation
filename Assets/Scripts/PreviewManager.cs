dec894706d5a0b4d996dec668c835155ab20b6f1
maceq687 <maciek@softman.pl>
2022-05-01 17:07:26 +0200

    Added PreviewManager

---
+++ Assets/Scripts/PreviewManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class PreviewManager : MonoBehaviour
+{
+    public PreviewManager MirrorPreview;
+    public GameObject WebCamPreview;
+    private Canvas VirtualMirrorCanvas;
+    private Camera VirtualMirrorCamera;
+    private InputDevice rightController;
+    private bool WebCam = true;
+    private bool VirtualCam = false;
+
+    
+    
+    void Start()
+    {
+        VirtualMirrorCanvas = MirrorPreview.GetComponentInChildren<Canvas>();
+        VirtualMirrorCamera = MirrorPreview.GetComponentInChildren<Camera>();
+        rightController = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+        VirtualMirrorCanvas.gameObject.SetActive(VirtualCam);
+        VirtualMirrorCamera.gameObject.SetActive(VirtualCam);
+    }
+
+    void Update()
+    {
+        bool triggerValue;
+        if (UnityEngine.InputSystem.Keyboard.current.gKey.wasPressedThisFrame || (rightController.TryGetFeatureValue(CommonUsages.primaryButton, out triggerValue) && triggerValue))
+        {
+            WebCam = !WebCam;
+            VirtualCam = !VirtualCam;
+            VirtualMirrorCanvas.gameObject.SetActive(VirtualCam);
+            VirtualMirrorCamera.gameObject.SetActive(VirtualCam);
+            WebCamPreview.SetActive(WebCam);
+        }
+    }
+}
